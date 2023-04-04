42ce75895532651d826548b4637a60fff699d1cc
anthony <anthony098626@gmail.com>
2023-04-04 21:02:27 +0000

    IPv4+DNS

--- IPv4-DNS.ps1
+++ IPv4-DNS.ps1
@@ -1,17 +1,17 @@
-# Replace USERNAME with the new user's username
-$Username = "USERNAME"
-
-# Replace NEW_IP_ADDRESS with the new user's IPv4 address
-$IpAddress = "NEW_IP_ADDRESS"
+# Input parameters
+$Username = Read-Host "Enter username"
+$IpAddress = Read-Host "Enter IPv4 address"
+$Gateway = Read-Host "Enter default gateway address"
+$Dns = Read-Host "Enter DNS server address"
 
 # Add the IPv4 address for the new user
-New-NetIPAddress -InterfaceAlias "Ethernet" -AddressFamily IPv4 -IPAddress $IpAddress -PrefixLength 24 -DefaultGateway "DEFAULT_GATEWAY_ADDRESS"
+New-NetIPAddress -InterfaceAlias "Ethernet" -AddressFamily IPv4 -IPAddress $IpAddress -PrefixLength 24 -DefaultGateway $Gateway
 
 # Set the DNS server address for the new user
-Set-DnsClientServerAddress -InterfaceAlias "Ethernet" -ServerAddresses "DNS_SERVER_ADDRESS"
+Set-DnsClientServerAddress -InterfaceAlias "Ethernet" -ServerAddresses $Dns
 
 # Set the user's IPv4 address configuration to static
-Set-NetIPInterface -InterfaceAlias "Ethernet" -InterfaceMetric 10 -AddressFamily IPv4 -Dhcp Disabled -IPAddress $IpAddress -PrefixLength 24 -DefaultGateway "DEFAULT_GATEWAY_ADDRESS"
+Set-NetIPInterface -InterfaceAlias "Ethernet" -InterfaceMetric 10 -AddressFamily IPv4 -Dhcp Disabled -IPAddress $IpAddress -PrefixLength 24 -DefaultGateway $Gateway
 
 # Add the new user to the local network access group
-Add-LocalGroupMember -Group "Network Configuration Operators" -Member $Username
+Add-LocalGroupMember -Group \"Network Configuration Operators" -Member $Username
