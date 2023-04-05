a226d3f09f0ec7cf91df7692698fcdc18ad5fa8a
user <robert.gregor11@gmail.com>
2023-04-05 14:34:56 +0000

    projv1

--- create_AD_user.ps1
+++ create_AD_user.ps1
@@ -6,6 +6,8 @@
     # [Press Any Key to Continue in PowerShell](https://java2blog.com/press-any-key-to-continue-powershell/)
     # [Get-ADUser](https://learn.microsoft.com/en-us/powershell/module/activedirectory/get-aduser?view=windowsserver2022-ps)
     # [Format-Table](https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/format-table?view=powershell-7.3)
+    # [PowerShell Looping: Understanding and Using Do…While](https://devblogs.microsoft.com/scripting/powershell-looping-understanding-and-using-do-while/)
+    
 
 function Create-NewUser {
     clear
@@ -19,11 +21,19 @@ function Create-NewUser {
     Write-Host "Passwords must be no less than seven characters and must include at least three of the following four character classes:"
     Write-Host "Uppercase letters (A-Z), Lowercase letters (a-z), Digits (0-9), Special characters (e.g., !@#$%^&*)"
     Write-Host "" 
-    $password = Read-Host -AsSecureString "Please enter $user_full_name's secure password"
+    
+    do {
+        $password = Read-Host -AsSecureString "Please enter $full_name's secure password"
+        $verify_password = Read-Host -AsSecureString "Please re-enter $full_name's secure password to verify"
+        if (($password | ConvertFrom-SecureString) -ne ($verify_password | ConvertFrom-SecureString)) {
+            Write-Host "Passwords do not match, please try again"
+        }
+    } while (($password | ConvertFrom-SecureString) -ne ($verify_password | ConvertFrom-SecureString))
+
     $company_name = Read-Host "Please enter the company name"
-    $office_location = Read-Host "Please enter $user_full_name's office location"
-    $dept_name = Read-Host "Please enter $user_full_name's department"
-    $job_title = Read-Host "Please enter $user_full_name's job title"
+    $office_location = Read-Host "Please enter $full_name's office location"
+    $dept_name = Read-Host "Please enter $full_name's department"
+    $job_title = Read-Host "Please enter $full_name's job title"
     Write-Host ""
 
     Read-Host "Press any key to create user..."
