16edc32d1a3d23c03ff0eaebaf03ff23fbf49297
jeremy patton <123398467+JeremyP1017@users.noreply.github.com>
2023-04-04 17:40:35 -0700

    Update Rename-winservVM.ps1

--- Rename-winservVM.ps1
+++ Rename-winservVM.ps1
@@ -1,19 +1,30 @@
 bash
 
-# Specify the old VM name and the new VM name
-$oldVMName = "OldVMName" $newVMName = "NewVMName" 
-# Connect to the Hyper-V server
-$serverName = "HypervServerName" $cred = Get-Credential Connect-VIServer -Server $serverName -Credential $cred
-# Get the old VM object and rename it 
-$vm = Get-VM -Name $oldVMName $vm | Set-VM -NewName $newVMName 
-# Disconnect from the Hyper-V server
-Disconnect-VIServer -Server $serverName -Force
+# Specify the old and new server names
+$OldServerName = "OldServerName"
+$NewServerName = "NewServerName"
 
+# Rename the server
+Rename-Computer -ComputerName $OldServerName -NewName $NewServerName -Restart
 
-To use this script, replace the OldVMName and NewVMName variables with the actual names
-of the VM before and after the rename operation, respectively. Then, replace the HypervServerName
-with the name or IP address of your Hyper-V server. Finally, run the script in a PowerShell console
-with administrative privileges. The script will prompt you for credentials to connect to the Hyper-V
-server, and then it will rename the specified VM.
+# Wait for the server to restart
+Start-Sleep-Seconds 60
+
+# Verify that the server name has changed
+if (Test-Connection -ComputerName $NewServerName -Count 1 -ErrorAction SilentlyContinue) {
+    Write-Host "Server has been renamed to $NewServerName and is online."
+} else {
+    Write-Host "Server rename failed."
+}
+
+This PowerShell script renames a Windows server by changing the computer name from $OldServerName to $NewServerName. The Rename-Computer 
+cmdlet is used to perform the actual renaming, and the -Restart switch is added to prompt the server to restart after the name change.
+
+After the restart, the script uses the Test-Connection cmdlet to check if the server is online with its new name. If the server is online, the
+script outputs a message indicating that the server has been renamed to the new name and is online. If the server is not online, the script outputs 
+a message indicating that the server rename has failed.
+
+The script also includes a 60-second pause (Start-Sleep -Seconds 60) to give the server enough time to complete the renaming process and restart.
+
 
 Sources :google,chatgrp
